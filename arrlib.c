49ec3f02a5a3ec3b5700a2b1289de36602c7fe87
Gracjan Baryla <barylagracjan@icloud.com>
2026-09-25 16:11:14 +0100

    feat: add array_every subroutine

--- arrlib.c
+++ arrlib.c
@@ -2,16 +2,34 @@
 #include <string.h>
 #include <stdio.h>
 
-void * array_at(void *arr, size_t element_size, size_t length, signed int index){
+void * array_at(const void *arr, const size_t element_size, const size_t length, signed int index){
     unsigned char *bytes = (unsigned char *) arr;
     if(index < 0) index = (length + index);
     
     return (void *) ((size_t) bytes + index * element_size);
 }
 
+// void * concat(){};
+
+// void * copyWithin(){};
+
+// void * entries(){};
+
+int array_every(const void *arr, const size_t element_size, const size_t length, int callback(void * element)){
+    unsigned char * bytes = (unsigned char *) arr; 
+
+    for(size_t idx = 0; idx < length; idx++){
+        if(callback(bytes + idx * element_size) == 1){
+           return 1; 
+        }
+    }
+
+    return 0;
+}
+
+
 int main(void){
     int arr[8] = { 2, 5, 9, 0, 3, 4, 5, 1};
 
-    printf("%d\n", *(int *) array_at(arr, sizeof(int), 8, 1));
     return 0;
 }
