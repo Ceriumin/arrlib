15496d18d94d6102e04bc4169453fe3b832ebd8f
Gracjan Baryla <barylagracjan@icloud.com>
2026-09-25 22:59:33 +0100

    feat: add array_fill coroutine

--- arrlib.c
+++ arrlib.c
@@ -27,9 +27,31 @@ int array_every(const void *arr, const size_t element_size, const size_t length,
     return 0;
 }
 
+void *array_fill(void *arr, const size_t element_size, const size_t length, const void *val, size_t start, size_t end){
+    unsigned char * bytes = (unsigned char*) arr;
+
+    if(start < 0 || start >= end) 
+        start = 0;
+    if(end > length || end < start) 
+        end = (int) length;
+
+    for(size_t idx = start; idx < end; idx++){
+        memcpy(bytes + idx * element_size, val, element_size);
+    }
+    
+    return arr;
+}
 
 int main(void){
     int arr[8] = { 2, 5, 9, 0, 3, 4, 5, 1};
+    int value = 5;
+    void *ptr = &value;
+
+    array_fill(arr, sizeof(int), 8, ptr, 3, 7);
+    
+    for(int i = 0; i < 7; i++){
+        printf("%d\n", arr[i]);
+    }
 
     return 0;
 }
